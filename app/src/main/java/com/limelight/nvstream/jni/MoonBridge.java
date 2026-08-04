9247825b0bc1fa3c9deef676f1391719ac025b83
Turan Asikoglu <874158+trexx@users.noreply.github.com>
2026-08-04 20:29:12 +0200

    Stage 1 upgrading

--- app/src/main/java/com/limelight/nvstream/jni/MoonBridge.java
+++ app/src/main/java/com/limelight/nvstream/jni/MoonBridge.java
@@ -223,10 +223,10 @@ public class MoonBridge {
 
     public static int bridgeDrSubmitDecodeUnit(byte[] decodeUnitData, int decodeUnitLength, int decodeUnitType,
                                                int frameNumber, int frameType, char frameHostProcessingLatency,
-                                               long receiveTimeMs, long enqueueTimeMs) {
+                                               long receiveTimeUs, long enqueueTimeUs) {
         if (videoRenderer != null) {
             return videoRenderer.submitDecodeUnit(decodeUnitData, decodeUnitLength,
-                    decodeUnitType, frameNumber, frameType, frameHostProcessingLatency, receiveTimeMs, enqueueTimeMs);
+                    decodeUnitType, frameNumber, frameType, frameHostProcessingLatency, receiveTimeUs, enqueueTimeUs);
         }
         else {
             return DR_OK;
